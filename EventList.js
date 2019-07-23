077f6e6355b13a4dfa6d83332d244429fda67dd0
Brian Dendauw <brian.dendauw@student.howest.be>
2019-07-23 14:46:59 +0200

    flatlist a json file on screen

--- EventList.js
+++ EventList.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
-
+import { FlatList, Text } from "react-native";
 class EventList extends Component {
+  state = {
+    events: []
+  };
+
+  componentDidMount() {
+    const allEvents = require("./db.json").events;
+    this.setState(prevState => ({
+      events: allEvents
+    }));
+  }
+
   render() {
-      return (
-          <Text>Hello</Text>
-      )
+    return (
+      <FlatList
+        data={this.state.events}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={item => item.id}
+      />
+    );
   }
 }
 
